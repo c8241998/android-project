691102ce833e27ec3d5073b6acc503b19ce0014e
569362884@qq.com <8241998czd>
2018-12-16 18:26:04 +0800

    code 3

--- MySportUI/app/src/main/java/cn/edu/bjtu/mysport/NewsFragment.java
+++ MySportUI/app/src/main/java/cn/edu/bjtu/mysport/NewsFragment.java
@@ -1,14 +1,16 @@
 package cn.edu.bjtu.mysport;
 
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.TextView;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,10 +25,22 @@ public class NewsFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        WebView webView = (WebView)view.findViewById(R.id.webview);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.hupu.com");
+
+        Uri uri = Uri.parse("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
+
+        VideoView videoView = (VideoView)view.findViewById(R.id.videoView );
+
+        //设置视频控制器
+        videoView.setMediaController(new MediaController(getContext()));
+
+        //播放完成回调
+        videoView.setOnCompletionListener( new MyPlayerOnCompletionListener());
+
+        //设置视频路径
+        videoView.setVideoURI(uri);
+
+        //开始播放视频
+        videoView.start();
     }
 
     @Override
@@ -35,4 +49,11 @@ public class NewsFragment extends Fragment {
         return inflater.inflate(R.layout.activity_news,container,false);
     }
 
+    class MyPlayerOnCompletionListener implements MediaPlayer.OnCompletionListener {
+
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            Toast.makeText(  getContext(), "播放完成了", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
